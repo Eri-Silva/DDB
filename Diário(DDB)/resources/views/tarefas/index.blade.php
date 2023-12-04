76b1a76d23e7f3e4b792210162d9372fe052831d
ERIELSON BRITO DA SILVA <silva.erielson@academico.ifrn.edu.br>
2023-12-04 20:22:24 -0300

    att

--- Diário(DDB)/resources/views/tarefas/index.blade.php
+++ Diário(DDB)/resources/views/tarefas/index.blade.php
@@ -27,16 +27,165 @@
 	  
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar ftco-navbar-light site-navbar-target" id="ftco-navbar">
 	    <div class="container">
-	      <a class="navbar-brand" href="index.html">DDB</a>
+	      <a class="navbar-brand" href="/">DDB</a>
 	    </div>
 	  </nav>
 
-	  
+                          
 
-  <!-- loader -->
-  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+  <section class="ftco-section contact-section ftco-no-pb" id="contact-section">
+      <div class="container">
+      	<div class="row justify-content-center mb-5 pb-3">
+          <div class="col-md-7 heading-section text-center ftco-animate fadeInUp ftco-animated">
+            <h1 class="big big-2">Tarefas</h1>
+            <h2 class="mb-4">Tarefas</h2>
 
+            
+          </div>
+          <div class="form-group">
+                <input type="submit" value="Adicionar +" class="btn btn-primary py-3 px-5">
+              </div>
+          </div>
+           
+        <div class="row d-flex contact-info mb-5">
+          
+       
 
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate fadeInUp ftco-animated">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-map-signs"></span>
+          		</div>
+          		<h3 class="mb-4">Lavar a louça</h3>
+	            <p>Todos os dias, depois do almoço</p>
+              <div class="form-group">
+                <input type="submit" value="Editar" class="btn btn-primary py-1 px-3">
+                <input type="submit" value="Excluir" class="btn btn-primary py-1 px-3">
+              </div>       
+	          </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate fadeInUp ftco-animated">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-map-signs"></span>
+          		</div>
+          		<h3 class="mb-4">Lavar a louça</h3>
+	            <p>Todos os dias, depois do almoço</p>
+              <div class="form-group">
+                <input type="submit" value="Editar" class="btn btn-primary py-1 px-3">
+                <input type="submit" value="Excluir" class="btn btn-primary py-1 px-3">
+              </div>       
+	          </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate fadeInUp ftco-animated">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-map-signs"></span>
+          		</div>
+          		<h3 class="mb-4">Lavar a louça</h3>
+	            <p>Todos os dias, depois do almoço</p>
+              <div class="form-group">
+                <input type="submit" value="Editar" class="btn btn-primary py-1 px-3">
+                <input type="submit" value="Excluir" class="btn btn-primary py-1 px-3">
+              </div>       
+	          </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate fadeInUp ftco-animated">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-map-signs"></span>
+          		</div>
+          		<h3 class="mb-4">Lavar a louça</h3>
+	            <p>Todos os dias, depois do almoço</p>
+              <div class="form-group">
+                <input type="submit" value="Editar" class="btn btn-primary py-1 px-3">
+                <input type="submit" value="Excluir" class="btn btn-primary py-1 px-3">
+              </div>       
+	          </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate fadeInUp ftco-animated">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-map-signs"></span>
+          		</div>
+          		<h3 class="mb-4">Lavar a louça</h3>
+	            <p>Todos os dias, depois do almoço</p>
+              <div class="form-group">
+                <input type="submit" value="Editar" class="btn btn-primary py-1 px-3">
+                <input type="submit" value="Excluir" class="btn btn-primary py-1 px-3">
+              </div>       
+	          </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate fadeInUp ftco-animated">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-map-signs"></span>
+          		</div>
+          		<h3 class="mb-4">Lavar a louça</h3>
+	            <p>Todos os dias, depois do almoço</p>
+              <div class="form-group">
+                <input type="submit" value="Editar" class="btn btn-primary py-1 px-3">
+                <input type="submit" value="Excluir" class="btn btn-primary py-1 px-3">
+              </div>       
+	          </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate fadeInUp ftco-animated">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-map-signs"></span>
+          		</div>
+          		<h3 class="mb-4">Lavar a louça</h3>
+	            <p>Todos os dias, depois do almoço</p>
+              <div class="form-group">
+                <input type="submit" value="Editar" class="btn btn-primary py-1 px-3">
+                <input type="submit" value="Excluir" class="btn btn-primary py-1 px-3">
+              </div>       
+	          </div>
+          </div>
+
+          
+        </div>
+
+        <div class="row no-gutters block-9">
+          <div class="col-md-6 order-md-last d-flex">
+            <form action="#" class="bg-light p-4 p-md-5 contact-form">
+              @csrf
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Tarefa">
+              </div>
+              <div class="form-group">
+                <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Descrição"></textarea>
+              </div>
+              <div class="form-group">
+                <input type="submit" value="Adicionar tarefa" class="btn btn-primary py-3 px-5">
+              </div>
+            </form>
+          
+          </div>
+
+          <div class="col-md-6 d-flex">
+          	<div class="img" style="background-image: url(images/capa_caderno.png);"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="ftco-footer ftco-section">
+      <div class="container">
+          <div class="col-md-12 text-center">
+
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright ©<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+        </div>
+      </div>
+    </footer>
   <script src="js/jquery.min.js"></script>
   <script src="js/jquery-migrate-3.0.1.min.js"></script>
   <script src="js/popper.min.js"></script>
@@ -51,6 +200,14 @@
   <script src="js/scrollax.min.js"></script>
   
   <script src="js/main.js"></script>
+
+  <script>
+    function mostrar_modal() {
+            let idModal = document.getElementById('caixa_lancamento');
+            let modal_lancamento = new bootstrap.Modal(idModal);
+            modal_lancamento.show();
+        }
+  </script>
     
   </body>
 </html>
